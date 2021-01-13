a9f5d35b7e2f3d23a2fdbcb20a941b1e2364eb54
Upamanyu Das <upamanyu.das.work@gmail.com>
2021-01-13 23:23:35 +0530

    Add troubleshooting code

--- scrape.py
+++ scrape.py
@@ -11,7 +11,7 @@ def getData(item):
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
         "DNT": "1",
-        "Connection": "keep-alive",
+        "Connection": "close",
         "Upgrade-Insecure-Requests": "1",
     }
 
@@ -19,22 +19,26 @@ def getData(item):
 
     soup = BeautifulSoup(res.content, "html.parser")
 
-    price = soup.find("span", attrs={"id": "priceblock_ourprice"}).text
-    price_array = price.split("\xa0")
-    price = " ".join(price_array)
-    price_num = float(price_array[1].replace(",", ""))
+    try:
+        price = soup.find("span", attrs={"id": "priceblock_ourprice"}).text
+        price_array = price.split("\xa0")
+        price = " ".join(price_array)
+        price_num = float(price_array[1].replace(",", ""))
 
-    product_title = soup.find(id="productTitle").text.strip()
-    time_stamp = datetime.now()
+        product_title = soup.find(id="productTitle").text.strip()
+        time_stamp = datetime.now()
 
-    data = {
-        "id": item["id"],
-        "product_name": product_title,
-        "price": price,
-        "price_num": price_num,
-        "time_stamp": time_stamp,
-        "url": item["url"],
-        "target_price": item["target_price"],
-    }
+        data = {
+            "id": item["id"],
+            "product_name": product_title,
+            "price": price,
+            "price_num": price_num,
+            "time_stamp": time_stamp,
+            "url": item["url"],
+            "target_price": item["target_price"],
+        }
 
-    return data
+        return data
+
+    except:
+        print(res.content)
